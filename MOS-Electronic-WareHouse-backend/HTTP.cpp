21d552fea3099ab122b094d2459d046994719258
MatveyTishin <MatveyTishin@yandex.ru>
2021-02-24 16:43:11 +0300

    fix http

--- MOS-Electronic-WareHouse-backend/HTTP.cpp
+++ MOS-Electronic-WareHouse-backend/HTTP.cpp
@@ -42,5 +42,34 @@ std::string GetJsonByHTTP(std::string ip, int PORT)
 std::string SendDataToServer(bool b);
 
 bool GetJsonByHTTP(std::string ip, int PORT, std::string data) {
+	CURL* curl;
+	CURLcode res;
 
+	/* In windows, this will init the winsock stuff */
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	/* get a curl handle */
+	curl = curl_easy_init();
+	if (curl) {
+		/* First set the URL that is about to receive our POST. This URL can
+		   just as well be a https:// URL if that is what should receive the
+		   data. */
+		curl_easy_setopt(curl, CURLOPT_URL, ip);
+		/* Now specify the POST data */
+		curl_easy_setopt(curl, CURLOPT_PORT, PORT);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+
+		/* Perform the request, res will get the return code */
+		res = curl_easy_perform(curl);
+		/* Check for errors */
+		if (res != CURLE_OK) {
+			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+			return false;
+		}
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+	}
+	curl_global_cleanup();
+	return true;
 }
